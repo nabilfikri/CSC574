54b60016a7ff6937af67fd6a0c7c7de577895d98
Nabil Fikri <nabilfikrijmdin@gmail.com>
2022-11-24 16:11:25 +0800

    updates

---
+++ Chapter05-Server-SideScripting/hello.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<head>
+    <title>Hello PHP!</title>
+</head>
+<body>
+
+    <h1>Introduction to PHP</h1>
+
+    <p><?php echo "Hello World!"; ?></p>
+
+    <p>This is HTML</p>
+</body>
+</html>
